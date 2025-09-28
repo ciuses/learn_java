b61e8eac6b88b88e814c376b52863d77372191c6
ciuse <ciuse@yandex.ru>
2025-09-28 14:40:14 +0700

    In this time output is not work

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
-
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Not Hello World! And it's yery sed.");
+        // System.out.println("Not Hello World! And it's yery sed.");
     }
 }
