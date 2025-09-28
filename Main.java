c43850d3c69309647b27d87afca329634129283c
ciuse <ciuse@yandex.ru>
2025-09-28 14:42:10 +0700

    Hello Kitty

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        // System.out.println("Not Hello World! And it's yery sed.");
+        System.out.println("Hello Kitty!");
     }
 }
