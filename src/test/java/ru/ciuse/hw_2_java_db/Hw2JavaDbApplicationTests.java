419a7cf0d96cf42272a2af179119e45c8e9d40fd
ciuse <ciuse@yandex.ru>
2025-10-12 13:28:51 +0700

    Добавил файлы проекта.

---
+++ src/test/java/ru/ciuse/hw_2_java_db/Hw2JavaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ciuse.hw_2_java_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2JavaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
