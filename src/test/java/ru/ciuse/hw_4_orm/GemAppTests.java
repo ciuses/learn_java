85a610a886cebbf913a88447b03f64c094b64fd2
ciuse <ciuse@yandex.ru>
2025-10-21 19:53:47 +0700

    На записывал руками немного данных.

--- src/test/java/ru/ciuse/hw_4_orm/GemAppTests.java
+++ src/test/java/ru/ciuse/hw_4_orm/GemAppTests.java
@@ -25,73 +25,100 @@ class GemAppTests {
     @Autowired
     PriceInterface priceInterface;
 
-    @Test
-    void addGems() {
-        Gem myGem1 = new Gem("пуь");
-        Gem myGem2 = new Gem("Брилиант");
-        gemInterface.saveAndFlush(myGem1);
-        gemInterface.saveAndFlush(myGem2);
-
-    }
-
-    @Test
-    void addGems2() {
-        Gem myGem1 = new Gem("Тестовый камень");
-        Occurrence ground = new Occurrence("Казахстан");
-        Owner myOwner = new Owner("Медведь");
-        Price myPrice = new Price(500);
-        Auction myAuc = new Auction("Крисити");
-
-        gemInterface.saveAndFlush(myGem1);
-        occurrenceInterface.saveAndFlush(ground);
-        ownerInterface.saveAndFlush(myOwner);
-        priceInterface.saveAndFlush(myPrice);
-        auctionInterface.saveAndFlush(myAuc);
-
-    }
-
-    @Test
-    void addGems3() {
-        Gem myGem1 = new Gem("Супер гем");
-        Occurrence ground = new Occurrence("Люксеембург");
-        Owner myOwner = new Owner("Самон");
-        Price myPrice = new Price(25);
-        Auction myAuc = new Auction("Солсбери");
-
-        myGem1.setOccurrence(ground);
-        myGem1.setType("Каменный");
-//        myGem1.setPrice(myPrice);
-        myGem1.setOwner(myOwner);
-
-        occurrenceInterface.saveAndFlush(ground);
-        ownerInterface.saveAndFlush(myOwner);
-        priceInterface.saveAndFlush(myPrice);
-        auctionInterface.saveAndFlush(myAuc);
-        gemInterface.saveAndFlush(myGem1);
-
-    }
+//    @Test
+//    void addGems() {
+//        Gem myGem1 = new Gem("пуь");
+//        Gem myGem2 = new Gem("Брилиант");
+//        gemInterface.saveAndFlush(myGem1);
+//        gemInterface.saveAndFlush(myGem2);
+//
+//    }
+//
+//    @Test
+//    void addGems2() {
+//        Gem myGem1 = new Gem("Тестовый камень");
+//        Occurrence ground = new Occurrence("Казахстан");
+//        Owner myOwner = new Owner("Медведь");
+//        Price myPrice = new Price(500);
+//        Auction myAuc = new Auction("Крисити");
+//
+//        gemInterface.saveAndFlush(myGem1);
+//        occurrenceInterface.saveAndFlush(ground);
+//        ownerInterface.saveAndFlush(myOwner);
+//        priceInterface.saveAndFlush(myPrice);
+//        auctionInterface.saveAndFlush(myAuc);
+//
+//    }
+//
+//    @Test
+//    void addGems3() {
+//        Gem myGem1 = new Gem("Супер гем");
+//        Occurrence ground = new Occurrence("Люксеембург");
+//        Owner myOwner = new Owner("Самон");
+//        Price myPrice = new Price(25);
+//        Auction myAuc = new Auction("Солсбери");
+//
+//        myGem1.setOccurrence(ground);
+//        myGem1.setType("Каменный");
+////        myGem1.setPrice(myPrice);
+//        myGem1.setOwner(myOwner);
+//
+//        occurrenceInterface.saveAndFlush(ground);
+//        ownerInterface.saveAndFlush(myOwner);
+//        priceInterface.saveAndFlush(myPrice);
+//        auctionInterface.saveAndFlush(myAuc);
+//        gemInterface.saveAndFlush(myGem1);
+//
+//    }
 
     @Test
     void gemExperements() {
-        Gem myGem = new Gem("Сапфир Бомбея");
-        Occurrence ground = new Occurrence("Шри-Ланка");
-        Owner myOwner = new Owner("Елизавета II");
-        Price myPrice = new Price(100000000);
-        Auction myAuc = new Auction("Солсбери");
+//        Gem myGem = new Gem("Сапфир Бомбея");
+//        Occurrence ground = new Occurrence("Шри-Ланка");
+//        Owner myOwner = new Owner("Елизавета II");
+//        Price myPrice = new Price(100000000);
+//        Auction myAuc = new Auction("Солсбери");
+//
+//        ground.setDate("01.01.1987");
+//        ground.setPlace("Деревня");
+//
+//        myOwner.setCity("Лондон");
+//        myOwner.setContact("+79610865432");
+//
+//        myPrice.setFinal_price(1000000000);
+//        myPrice.setCurrency("USD");
+//
+//        myAuc.setCity("Нью Йорк");
+//        myAuc.setDate("01.11.2025");
+//
+//        myGem.setType("Чистый Сапфир");
+//        myGem.setOwner(myOwner);
+//        myGem.setOccurrence(ground);
+//
+//        myPrice.setGem(myGem);
+//
+//        myAuc.setGem(myGem);
 
-        myGem.setType("Чистый Сапфир");
-        ground.setDate("01.01.1987");
-        ground.setPlace("Деревня");
 
-        myOwner.setCity("Лондон");
-        myOwner.setContact("+79610865432");
+        Gem myGem = new Gem("Янтарь");
+        Occurrence ground = new Occurrence("Эстония");
+        Owner myOwner = new Owner("Калис");
+        Price myPrice = new Price(50);
+        Auction myAuc = new Auction("Московская биржа");
 
-        myPrice.setFinal_price(1000000000);
-        myPrice.setCurrency("USD");
+        ground.setDate("24.05.2014");
+        ground.setPlace("Рига");
 
-        myAuc.setCity("Нью Йорк");
-        myAuc.setDate("01.11.2025");
+        myOwner.setCity("Серпухово");
+        myOwner.setContact("+34507132211");
 
+        myPrice.setFinal_price(100);
+        myPrice.setCurrency("TG");
+
+        myAuc.setCity("Дублин");
+        myAuc.setDate("03.02.2026");
+
+        myGem.setType("Там ДНК");
         myGem.setOwner(myOwner);
         myGem.setOccurrence(ground);
 
@@ -100,11 +127,6 @@ class GemAppTests {
         myAuc.setGem(myGem);
 
 
-//        occurrenceInterface.save(ground);
-//        ownerInterface.save(myOwner);
-//        priceInterface.save(myPrice);
-//        auctionInterface.save(myAuc);
-//        gemInterface.save(myGem);
 
         occurrenceInterface.saveAndFlush(ground);
         ownerInterface.saveAndFlush(myOwner);
